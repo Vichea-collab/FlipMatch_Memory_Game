75f03ef1fed666327a1118ffed69a0950b0b9da6
Vichea-collab <luchvichea43@gmail.com>
2026-01-03 15:19:35 +0700

    update usecase and data

--- lib/domain/usecases/delete_player.dart
+++
@@ -1,9 +0,0 @@
-import '../repositories/player_repository.dart';
-
-class DeletePlayer {
-  final PlayerRepository repository;
-
-  DeletePlayer(this.repository);
-
-  Future<void> call(String name) => repository.deletePlayer(name);
-}
